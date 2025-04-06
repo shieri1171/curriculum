550e8dcb30df9cb6c627aba7a532353be5d16fbe
shieri1171 <shieri.1171@gmail.com>
2025-04-07 01:44:06 +0900

    20250407 - トップページ表示 - 実装中

--- app/app/Http/Controllers/DisplayController.php
+++ app/app/Http/Controllers/DisplayController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 class DisplayController extends Controller
 {
     public function index() {
-        return view('top');
+
+        $item = Item::all();
+        
+        return view('top',[
+            'items'=>$items,
+        ]);
     }
 }
